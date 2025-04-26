f0e744b1845cf5a8ea79a77b3c4dc67623521e8e
Benjamín Peralta <benjaminp158@gmail.com>
2025-04-26 19:10:04 -0300

    Added UsuarioRouter

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ from Modelos.Producto import Producto
 from Modelos.Venta import Venta
 from Modelos.Usuario import Usuario
 from Modelos.Venta import Venta
-from Routers import ProductoRouter, CategoríaRouter
+from Routers import ProductoRouter, CategoríaRouter, UsuarioRouter
 
 app = FastAPI()
 
 app.title = "Trabajo práctico NRO 2"
 app.version = "0.0.∞"
 
-app.router.include_router(ProductoRouter.router, "/productos")
-app.router.include_router(CategoríaRouter.router, "/categorias")
+app.router.include_router(ProductoRouter.router, prefix="/productos")
+app.router.include_router(CategoríaRouter.router, prefix="/categorias")
+app.router.include_router(UsuarioRouter.router, prefix="/usuarios")
