c18c32225d1f1bb91f6e35db246ba249cbdf769e
Benjamín Peralta <benjaminp158@gmail.com>
2025-04-26 10:55:41 -0300

    Added additional validation

--- Modelos/Categoría.py
+++ Modelos/Categoría.py
@@ -1,6 +1,12 @@
-from pydantic import BaseModel, PositiveInt
+from pydantic import BaseModel, Field, PositiveInt, field_validator
 
 
 class Categoría(BaseModel):
     id: PositiveInt
-    descripción: str
+    descripción: str = Field(min_length=1)
+
+    @field_validator("descripción")
+    def validar_descripción_en_blanco(cls, valor: str):
+        if valor.isspace():
+            raise ValueError("La descripción no debe estar vacía")
+        return valor
