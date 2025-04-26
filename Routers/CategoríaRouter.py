d60e4fee2a146dc536c35136938516984918b946
Benjamín Peralta <benjaminp158@gmail.com>
2025-04-26 10:57:05 -0300

    Added each router with they own logic

---
+++ Routers/CategoríaRouter.py
@@ -0,0 +1,48 @@
+from http.client import HTTPException
+from fastapi import APIRouter, status, Body
+from Modelos.Categoría import Categoría
+from BD.bd import categorías
+
+router = APIRouter()
+
+
+# Lo hago así profe para que esté más ordenado
+
+
+@router.get("/", tags=["Productos"], response_model=list[Categoría])
+def obtener_categorías() -> list[dict]:
+    return categorías
+
+
+@router.get("/{id}", tags=["Categorías"], response_model=Categoría)
+def obtener_categoría_por_id(id: int):
+    for categoría in categorías:
+        if categoría["id"] == id:
+            return categoría
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND, detail="Categoría no encontrado"
+    )
+
+
+@router.post("/", tags=["Categorías"], status_code=status.HTTP_201_CREATED)
+def agregar_categoría(categoría: Categoría):
+    categorías.append(categoría.__dict__)
+
+
+@router.put("/{id}", tags=["Categorías"], response_model=Categoría)
+def modificar_categoría(id, categoría: Categoría = Body()):
+    categoría_seleccionado = obtener_categoría_por_id(id)
+    try:
+        i = categorías.index(categoría_seleccionado)
+    except IndexError:
+        return
+
+    categorías[i] = categoría.__dict__
+    return categoría.__dict__
+
+
+@router.delete("/{id}", tags=["Categorías"], status_code=status.HTTP_204_NO_CONTENT)
+def eliminar_categoría(id: int):
+    categoría_seleccionado = obtener_categoría_por_id(id)
+
+    categorías.remove(categoría_seleccionado)
---
+++ Routers/ProductoRouter.py
@@ -0,0 +1,72 @@
+from http.client import HTTPException
+from fastapi import APIRouter, status, Body
+from Modelos.Producto import Producto
+from BD.bd import productos
+from Servicios.CategoríaServicio import existe_categoría_con_id
+
+router = APIRouter()
+
+
+# Lo hago así profe para que esté más ordenado
+@router.get("/")
+def hello_world():
+    return {"hello": "word"}
+
+
+@router.get("/", tags=["Productos"], response_model=list[Producto])
+def obtener_productos() -> list[dict]:
+    return productos
+
+
+@router.get("/{id}", tags=["Productos"], response_model=Producto)
+def obtener_producto_por_id(id: int):
+    for producto in productos:
+        if producto["id"] == id:
+            return producto
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND, detail="Producto no encontrado"
+    )
+
+
+@router.post("/", tags=["Productos"], status_code=status.HTTP_201_CREATED)
+def agregar_producto(producto: Producto):
+    if existe_categoría_con_id(producto.id_categoría):
+        productos.append(producto.__dict__)
+        return
+    raise HTTPException(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        detail="No se ha podido crear. Categoría no existente",
+    )
+
+
+@router.put("/{id}", tags=["Productos"], response_model=Producto)
+def modificar_producto(id, producto: Producto = Body()):
+    producto_seleccionado = obtener_producto_por_id(id)
+    try:
+        i = productos.index(producto_seleccionado)
+    except IndexError:
+        return
+    if existe_categoría_con_id(productos[i].id_categoría):
+        productos[i] = producto.__dict__
+        return producto.__dict__
+    raise HTTPException(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        detail="No se ha podido modificar. Categoría no existente",
+    )
+
+
+@router.delete("/{id}", tags=["Productos"], status_code=status.HTTP_204_NO_CONTENT)
+def eliminar_producto(id: int):
+    producto_seleccionado = obtener_producto_por_id(id)
+
+    productos.remove(producto_seleccionado)
+
+
+@router.get("/", tags=["Productos"], response_model=Producto)
+def obtener_producto_por_categoría(categoría: str):
+    for producto in productos:
+        if producto["categoría"].casefold() == categoría.casefold():
+            return producto
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND, detail="Producto no encontrado"
+    )
